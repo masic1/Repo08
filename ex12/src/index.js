c7d86df0f554a0bb83f6560bc99536a3153e3a0b
masic1 <masic.mirza@gmail.com>
2021-09-21 00:14:28 +0200

    Added ex11-16

---
+++ ex12/src/index.js
@@ -0,0 +1,8 @@
+// Only change below this line
+function randomInteger() {
+    return Math.floor(Math.random() * 30);
+}
+
+console.log(randomInteger());
+// Only change above this line
+module.exports = randomInteger;
